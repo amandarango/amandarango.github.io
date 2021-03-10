acabdf6fcb7b2119a4c7427158ca4ae493c0d3b6
heyitsbuse <75186522+heyitsbuse@users.noreply.github.com>
2021-03-10 17:57:05 -0500

    Big push
    Footer is designed, about page, faq page, and landing page is being worked on.

--- js/script.js
+++ js/script.js
@@ -10,6 +10,53 @@ function myFunction() {
   }
 }
 
+// This brings functionality to landing page email sign up
+function saveToFirebase()
+{
+    const data = document.getElementById('emailForm').elements[0].value;
+
+
+  if(ValidateEmail(data))
+  {
+    database.ref('emails').push({ email: data}).then(function(snapshot) {
+      pass(); // some success method
+      }, function(error)
+         {
+            console.log('error' + error);
+            fail(); // some error method
+          });
+     document.getElementById('frm1').reset();
+  }
+
+}
+
+function ValidateEmail(mail)
+{
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail))
+        {
+          pass();
+          return (true);
+        }
+    else{
+          fail();
+          return (false);
+          }
+}
+
+
+function fail()
+{
+
+  $('#fail').finish().show().delay(1000).fadeOut(6000);
+
+}
+function pass()
+{
+
+$('#success').finish().show().delay(1000).fadeOut(6000);
+
+}
+
 let widthMatch = window.matchMedia("(min-width: 811px)");
 // mm in the function arg is the matchMedia object, passed back into the function
 widthMatch.addEventListener('change', function(mm) {
